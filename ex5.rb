6cf1ef23f8bb6dd05e19107becba252b99b33cff
A.T. Kell <alex@alexkell.com>
2019-07-13 09:32:05 -0400

    Completed exercise.

--- ex5.rb
+++ ex5.rb
@@ -2,20 +2,26 @@ puts "-" * 30
 puts "Exercise 5: More Variables and Printing"
 puts "-" * 30
 
-my_name = "Zed A. Shaw"
-my_age = 35 # not a lie in 2009
-my_height = 74 #inches
-my_weight = 180 # lbs
-my_eyes = "Blue"
-my_teeth = "White"
-my_hair = "Brown"
+name = "Zed A. Shaw"
+age = 35 # not a lie in 2009
+height = 74 #inches
+inches_per_foot = 12
+height_in_feet = height / inches_per_foot
+centimers_per_inch = 2.54
+height_in_centimers = inches_per_foot * centimers_per_inch
+weight = 180 # lbs
+pounds_per_kilo = 2.2
+weight_in_kilos = weight / pounds_per_kilo
+eyes = "Blue"
+teeth = "White"
+hair = "Brown"
 
-puts "Let's talk about #{my_name}."
-puts "He's #{my_height} inches tall."
-puts "He's #{my_weight} pounds heavy."
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall (or #{height_in_feet} feet or #{height_in_centimers} centimers)."
+puts "He's #{weight} pounds heavy (or #{weight_in_kilos} kilograms)."
 puts "Actually, that's not too heavy."
-puts "He's got #{my_eyes} eyes and #{my_hair} hair, too."
-puts "His teeth are usually #{my_teeth} depending on the coffee."
+puts "He's got #{eyes} eyes and #{mhir} hair, too."
+puts "His teeth are usually #{teeth} depending on the coffee."
 
 # this line is tricky, try to get it right!
-puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}"
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}"
