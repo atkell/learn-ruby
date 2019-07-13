82366a32c14f5fbb2715723ef655f6ca572350b3
A.T. Kell <alex@alexkell.com>
2019-07-13 09:33:36 -0400

    Completed study drills.

--- ex5.rb
+++ ex5.rb
@@ -8,7 +8,7 @@ height = 74 #inches
 inches_per_foot = 12
 height_in_feet = height / inches_per_foot
 centimers_per_inch = 2.54
-height_in_centimers = inches_per_foot * centimers_per_inch
+height_in_centimers = height * centimers_per_inch
 weight = 180 # lbs
 pounds_per_kilo = 2.2
 weight_in_kilos = weight / pounds_per_kilo
@@ -20,7 +20,7 @@ puts "Let's talk about #{name}."
 puts "He's #{height} inches tall (or #{height_in_feet} feet or #{height_in_centimers} centimers)."
 puts "He's #{weight} pounds heavy (or #{weight_in_kilos} kilograms)."
 puts "Actually, that's not too heavy."
-puts "He's got #{eyes} eyes and #{mhir} hair, too."
+puts "He's got #{eyes} eyes and #{hair} hair, too."
 puts "His teeth are usually #{teeth} depending on the coffee."
 
 # this line is tricky, try to get it right!
