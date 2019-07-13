e4b54e949651d1e6ff4d93c870af788e9a9ee100
A.T. Kell <alex@alexkell.com>
2019-07-12 22:46:07 -0400

    Completed study drill exercise.

---
+++ calc-distance-run.rb
@@ -0,0 +1,13 @@
+# Part of the study drills for ex3 from Learn Ruby the Hard Way
+# Find something you need to calculate and write a new .rb file that does it.
+
+# Print a string that includes a basic computation
+puts "If I run at a pace of 2 miles per hour, how far will I run in 15 minutes? #{2.00 * 15 / 60} miles"
+
+puts "What about if I run at a pace of 4.5 miles per hour? #{4.50 * 15 / 60} miles"
+
+# 1 mile is 5,280 feet.
+# 1 mile in 10 minutes (which is 10 / 60 minutes) or 1/6 of an hour
+# pace = distance over time
+# pace = 1 / (1 / 6) or 1 * 6
+puts "What pace is required to run a mile in 10 minutes? #{1.0 * 6} mph"
