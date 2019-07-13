c38db5284c2c48a670366f82e5b1bd2a30401722
A.T. Kell <alex@alexkell.com>
2019-07-12 22:32:12 -0400

    Completed exercise.

---
+++ ex3.rb
@@ -0,0 +1,41 @@
+# Print a string
+puts "I will now count my chickens:"
+
+# Print a string that also includes a computation
+puts "Hens #{25.0 + 30 / 6}"
+
+# Print a string that also includes a computation
+puts "Roosters #{100.0 - 25 * 3 % 4}"
+
+# Print a string
+puts "I will now count the eggs:"
+
+# Print an integer, the result of a computation
+puts 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Print a string
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Prints an integer, the result of a computation
+puts 3.0 + 2 < 5 - 7
+
+# Print a string that also includes a computation
+puts "What is 3 + 2? #{3.0 + 2}"
+
+# Print a string that also includes a computation
+puts "What is 5 - 7? #{5.0 - 7}"
+
+# Print a string
+puts "Oh, that's why it's false."
+
+# Print a string
+puts "How about some more."
+
+# Print a string that also includes a computation
+puts "Is it greater? #{5.0 > -2}"
+
+# Print a string that also includes a computation
+puts "Is it greater or equal? #{5.0 >= -2}"
+
+# Print a string that also includes a computation
+puts "Is it less or equal? #{5 <= -2.0}"
