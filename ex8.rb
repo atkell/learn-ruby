ddb562fc1116483dd1e35cbb99118681c89acd57
A.T. Kell <alex@alexkell.com>
2019-07-16 23:16:19 -0400

    Completed study drills for exercise 8.

--- ex8.rb
+++ ex8.rb
@@ -2,13 +2,22 @@ puts "-" * 30
 puts "Exercise 8: Printing, Printing"
 puts "-" * 30
 
+# Assign a string containing named values to the variable formatter
 formatter = "%{first} %{second} %{third} %{fourth}"
 
+# Pass the values included in this string into the format string.
 puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+
+# Pass the values included in this string into the format string.
 puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
+
+# Pass the values included in this string into the format string. Note, we're using boolean values here and not strings.
 puts formatter % {first: true, second: false, third: true, fourth: false}
+
+# Pass the values included in this string into the format string. Note, this is a bit circular as we're passing in the value assigned to formatter into the format string named formatter.
 puts formatter % {first: formatter, second: formatter, third: formatter,    fourth: formatter}
 
+# An alternate way to write this format string that is arguably easier to read
 puts formatter % {
     first: "I had this thing.",
     second: "That you could type up right.",
