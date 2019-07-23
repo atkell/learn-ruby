dfc2f827e9f34b9767f23003b692743b6b054b58
A.T. Kell <alex@alexkell.com>
2019-07-23 12:32:10 -0400

    Completed exercise 13

---
+++ ex13.rb
@@ -0,0 +1,9 @@
+puts "-" * 30
+puts "Exercise 13: Parameters, Unpacking, Variables"
+puts "-" * 30
+
+first, second, third = ARGV
+
+puts "Your first variable is #{first}"
+puts "Your second variable is #{second}"
+puts "Your third variable is #{third}"
