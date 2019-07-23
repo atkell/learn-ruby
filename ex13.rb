85a9e7c43891b1eb570eabfb439611ca2a602286
A.T. Kell <alex@alexkell.com>
2019-07-23 12:38:55 -0400

    Completed study drills for exercise 13.

--- ex13.rb
+++ ex13.rb
@@ -2,8 +2,16 @@ puts "-" * 30
 puts "Exercise 13: Parameters, Unpacking, Variables"
 puts "-" * 30
 
-first, second, third = ARGV
+# first, second, third = ARGV
 
-puts "Your first variable is #{first}"
-puts "Your second variable is #{second}"
-puts "Your third variable is #{third}"
+# puts "Your first variable is #{first}"
+# puts "Your second variable is #{second}"
+# puts "Your third variable is #{third}"
+
+# Exericse 13 Study Drills
+user, user2 = ARGV
+
+puts "Hello friend, I see your name is #{user}"
+# print "but where are you from? "
+# # home = $stdin.gets.chomp
+# puts "Ah, great, so your name is #{name} and your hometown is #{home}."
