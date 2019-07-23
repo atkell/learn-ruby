76f38325ab5c9f8627a756703336d8c101ff5060
A.T. Kell <alex@alexkell.com>
2019-07-23 12:53:06 -0400

    Completed study drills for exercise 14.

--- ex14.rb
+++ ex14.rb
@@ -2,17 +2,19 @@ puts "-" * 30
 puts "Exercise 14: Prompting and Passing"
 puts "-" * 30
 
-user_name = ARGV.first # this could be how to avoid returning ['string']
-prompt = "> "
+user_name, fav_color = ARGV # this could be how to avoid returning ['string']
+prompt = "$ "
 
-puts "Hi #{user_name}."
+puts "Hi #{user_name}. I see you also like the color #{fav_color}.",
+     "Tip: If your favorite color is empty, you didn't include it as an argument."
+
 puts "I'd like to ask you a few questions, friend."
-puts "Do you like me #{user_name}?"
-puts prompt
+
+puts "Do you like me #{user_name}?", prompt
 likes = $stdin.gets.chomp
 
-puts "Where do you live #{user_name}? "
-puts prompt
+
+puts "Where do you live #{user_name}? ", prompt
 lives = $stdin.gets.chomp
 
 # a comma for puts is like using it twice
@@ -22,5 +24,6 @@ computer = $stdin.gets.chomp
 puts """
 Alright, you said #{likes} about liking me.
 You live in #{lives}. Not sure where that is.
+Your favorite color is #{fav_color}.
 And you have a #{computer} computer. Nice.
 """
