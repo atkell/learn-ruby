352dc81270cc6544c3f93756d9bf0ef33cc9df93
A.T. Kell <alex@alexkell.com>
2019-07-25 18:48:16 -0400

    Completed exercise 16 study drills.

--- ex16.rb
+++ ex16.rb
@@ -5,16 +5,19 @@ puts "-" * 30
 filename = ARGV.first
 
 puts "We're going to erase #{filename}"
+puts "But first, let's preview the first few lines."
+input = open(filename)
+print(input.read)
 puts "If you don't want that, hit CTRL-C (^C)."
 puts "If you do want that, hit RETURN."
 
 $stdin.gets
 
-puts "Opening the file..."
+puts "Opening the file (which also truncates it)"
 target = open(filename, 'w')
 
-puts "Truncating the file. Goodbye!"
-target.truncate(0)
+# puts "Truncating the file. Goodbye!"
+# target.truncate(0)
 
 puts "Now I'm going to ask you for three lines."
 
@@ -27,12 +30,7 @@ line3 = $stdin.gets.chomp
 
 puts "I'm going to write these to a file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(line1 + "\n" + line2 + "\n" + line3 + "\n")
 
 puts "And finally, we close it."
 target.close
