ee8c91d8c56b866a241b0094aa4e6883cf912c5d
A.T. Kell <alex@alexkell.com>
2019-07-25 19:03:46 -0400

    Completed exercise 17.

--- ex17.rb
+++ ex17.rb
@@ -7,9 +7,9 @@ from_file, to_file = ARGV
 puts "Copying from #{from_file} to #{to_file}"
 
 # we could do these two on a single line, how?
-in_file = open(from_file)
-indata = in_file.read
-# indata = from_file.read
+# in_file = open(from_file)
+# indata = in_file.read
+indata = from_file.read
 
 puts "The input file is #{indata.length} bytes long."
 
